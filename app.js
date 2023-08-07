9fb916a6c41ac05bf5fe0f8c883e516b6028c86a
sree-r-one <sreeraj_ec@hotmail.com>
2023-08-08 00:37:00 +0800

    pushing changes to git

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require("http");
 
-const server = http.createServer((req, res) => {
-  res.end("Hello World");
-});
+const server = http.createServer((req, res) => {});
 
-server.listen(3000);
+server.listen(3000, () => {
+  console.log("Server is listening on PORT 3000");
+});
